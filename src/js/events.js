bb6e6cf8b3580664c7913ec47c85044df98dff64
bbeetlesam <samsismines@gmail.com>
2025-06-12 17:39:20 +0700

    refactor: move hash preventer to be inside setupEvents

--- src/js/events.js
+++ src/js/events.js
@@ -19,15 +19,15 @@ export function setupEvents() {
         document.getElementById('itch').addEventListener('click', reload);
         document.getElementById('instagram').addEventListener('click', ig);
     });
-}
-
-// Prevent scrolling hash ref in the page url
-document.querySelectorAll('.navbar-link').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
+    
+    // Prevent scrolling hash ref in the page url
+    document.querySelectorAll('.navbar-link').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
+}
